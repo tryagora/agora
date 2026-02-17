e9b1b2d9d119d0b1837b13f0e626e0b73ac2895e
Afonso Pereira <afonso@afpereira.me>
2026-02-17 23:36:00 +0000

    add missing tauri 2 template files

--- clients/mobile/src-tauri/src/main.rs
+++ clients/mobile/src-tauri/src/main.rs
@@ -1,5 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    tauri::builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  app_lib::run();
 }
